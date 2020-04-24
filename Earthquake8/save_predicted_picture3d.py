69bb17ddd904eaee1b4cc1c7a61188558a415959
taoshutong <62104815+taoshutong@users.noreply.github.com>
2020-04-24 10:37:37 +0800

    Update save_predicted_picture3d.py

--- Earthquake8/save_predicted_picture3d.py
+++ Earthquake8/save_predicted_picture3d.py
@@ -1,7 +1,8 @@
 import torch
 from functions import *
 import os
-import matplotlib; matplotlib.use('TkAgg')
+import matplotlib; 
+#matplotlib.use('TkAgg')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # os.environ["CUDA_VISIBLE_DEVICES"] = "2"  # Only GPU 1 is visible to this code
 import data_loader.dataloader3 as module_data
@@ -15,7 +16,7 @@ np.random.seed(1)
 random.seed(1)
 args ={"seismic_path":"data/FYP_data/seis_sub_350IL_500t_1200XL.npy",
             "label_path": "data/FYP_data/fault_sub_350IL_500t_1200XL.npy",
-            "batch_size": 1,
+            "batch_size": 8,
             "test_number_of_pictures": 64,
             "test_start": 286,
             "dimension": 0
@@ -26,8 +27,8 @@ def main():
     test_set_loader = test_set.test_loader
     model = unet3d()
 
-    model.load_state_dict(torch.load('model_best1.pth',map_location=torch.device('cpu'))['state_dict'])
-    cube = np.zeros((64,352,352))
+    model.load_state_dict(torch.load('saved/models/unet3d/0423_104719/model_best.pth')['state_dict'])
+    cube = np.zeros((64,500,1200))
     blocks = []
     print('-'*30)
 
@@ -35,10 +36,16 @@ def main():
     for images in test_set_loader:
         print(cnt)
         cnt+=1
-        images = Variable(images.cuda())
+        images = Variable(images)
         outputs = model(images)
-        tosave = torch.stack(outputs).detach().cpu().numpy()
-        blocks.append(tosave)
+        outputs = torch.squeeze(outputs)
+        blocks.extend(outputs.detach().cpu())
+        
+        # blocks.append(tosave)
+    blocks = torch.stack(blocks).numpy()
+    blocks = np.squeeze(blocks)
+    print(blocks.shape)
+
 
     r = 32
     step = 48
@@ -46,10 +53,10 @@ def main():
     for i in range(r, cube.shape[0] - r + 1, step):
         for j in range(r, cube.shape[1] - r + 1, step):
             for k in range(r, cube.shape[2] - r + 1, step):
-                cube[i - r:i + r, j - r:j + r, k - r:k + r]+=blocks[x].detach().numpy()
+                cube[i - r:i + r, j - r:j + r, k - r:k + r]+=blocks[x]
                 x=x+1
         # print(res.shape)
-    cube_overlap = np.zeros((64,352,352))
+    cube_overlap = np.zeros((64,500,1200))
 
     r = 32
     step = 48
@@ -60,10 +67,12 @@ def main():
                 cube_overlap[i - r:i + r, j - r:j + r, k - r:k + r]+=np.ones((64,64,64))
                 x=x+1
 
+
     cube=np.true_divide(cube,cube_overlap)
 
+
     #run_id = datetime.now().strftime(r'%m%d_%H%M%S')
-    save_path ='saved/picture/unet3d/new1'
+    save_path ='saved/picture/unet3d/new'
 
     seismic_path = 'data/FYP_data/seis_sub_350IL_500t_1200XL.npy'
     label_path = 'data/FYP_data/fault_sub_350IL_500t_1200XL.npy'
@@ -73,25 +82,31 @@ def main():
 
     print('-'*30)
     for index in range(0,64):
-        a = cube[index,0:352,0:352]
+        a = cube[index,:,:]
+
 
         heatmap_img = cv2.applyColorMap((a * 255).astype(np.uint8), cmapy.cmap('jet_r'))
         plt.figure(figsize=(10, 8))
         plt.imshow(heatmap_img)
         plt.axis('off')
         plt.savefig('{}/predicted_{}.png'.format(save_path,str(index)))
-        plt.imshow(seismic[index+286,0:352,0:352])
+        plt.imshow(seismic[index+286,:,:])
         plt.axis('off')
         plt.savefig('{}/seismic_{}.png'.format(save_path,str(index)))
-        plt.imshow(fault[index+286,0:352,0:352])
+        plt.imshow(fault[index+286,:,:])
         plt.axis('off')
         plt.savefig('{}/fault_{}.png'.format(save_path,str(index)))
         # save path
         print('{}/predicted_{}.png'.format(save_path,str(index)))
 
+
+
         a=blendTwoImages('{}/fault_{}.png'.format(save_path,str(index)),'{}/seismic_{}.png'.format(save_path,str(index)),'{}/pre_fusion_{}.png'.format(save_path,str(index)))
         b=blendTwoImages(a,'{}/predicted_{}.png'.format(save_path,str(index)),'{}/fusion_{}.png'.format(save_path,str(index)))
-
+        #fusion path
+        # print(b)
+        # a=blendTwoImages(label,'{}/{}_{}.png'.format(save_path,run_id,str(index)),'saved/ab.png')
+        # b=blendTwoImages(a,seismic)
 
 if __name__ == '__main__':
     main()
