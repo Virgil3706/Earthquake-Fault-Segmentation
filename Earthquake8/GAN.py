8ba59e0df4e24f2d68531d0722d2b1b442db0758
Virgil <61879852+Virgil3706@users.noreply.github.com>
2020-04-27 18:49:27 +0800

    Version8_GAN added
    GAN added for transfer low_resolution seismic pictures to high_resolution seismic pictures

---
+++ Earthquake8/GAN.py
@@ -0,0 +1,329 @@
+import argparse
+import collections
+import torch
+import numpy as np
+import data_loader.dataloader3 as module_data
+import model.loss as module_loss
+import model.metric as module_metric
+import model.model as module_arch
+from functions import *
+from parse_config import ConfigParser
+from trainer import Trainer
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# fix random seeds for reproducibility
+SEED = 123
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(SEED)
+splitsize = 96
+
+from torchvision import transforms
+from torchvision import datasets
+from torchvision.utils import save_image
+
+
+def main(config):
+    logger = config.get_logger('train')
+    # setup data_loader instances
+
+    # build model architecture, then print to console
+
+    model = config.init_obj('arch', module_arch)
+    print('Generator:')
+    logger.info(model)
+    # print(config['arch']['type'])
+    # summary(model, (1, splitsize, splitsize))
+
+    # get function handles of loss and metrics
+    # criterion = getattr(module_loss, config['loss'])
+    # # print(criterion)
+    # metrics = [getattr(module_metric, met) for met in config['metrics']]
+    #
+    # # build optimizer, learning rate scheduler. delete every lines containing lr_scheduler for disabling scheduler
+    # trainable_params = filter(lambda p: p.requires_grad, model.parameters())
+    # optimizer = config.init_obj('optimizer', torch.optim, trainable_params)
+    #
+    # #optimizer = config.init_obj('optimizer2', module_opti, trainable_params)
+    #
+    # lr_scheduler = config.init_obj('lr_scheduler2', torch.optim.lr_scheduler, optimizer)
+    # trainer = Trainer(model, criterion, metrics, optimizer,
+    #                   config=config,
+    #                   data_loader=data_loader,
+    #                   valid_data_loader=valid_data_loader,
+    #                   lr_scheduler=lr_scheduler)
+
+    import torch
+    import torch.nn as nn
+    from torch.autograd import Variable
+    from torch.utils.data import DataLoader
+    from torchvision import transforms
+    from torchvision import datasets
+    from torchvision.utils import save_image
+    import os
+
+    if not os.path.exists('./dc_img'):
+        os.mkdir('./dc_img')
+
+    batch_size = 128
+    num_epoch = 1000
+
+    z_dimension = 100  # noise dimension
+
+    # setup data_loader instances
+    data_loader = config.init_obj('data_loader', module_data)
+    data_loader, valid_data_loader = data_loader.train_loader, data_loader.val_loader
+    data_loader = valid_data_loader
+    h = 96;
+    w = 96;
+
+    # for i, (img, label) in enumerate(data_loader):
+    #     h=img.shape[2]
+    #     w=img.shape[3]
+    # print("h",h)
+    # print("w",w)
+
+    def to_img(x):
+        out = 0.5 * (x + 1)
+        out = out.clamp(0, 1)
+        out = out.view(-1, 1, h, w)
+        return out
+
+    class discriminator2(nn.Module):
+
+        def __init__(self):
+            super(discriminator, self).__init__()
+            self.conv1 = nn.Sequential(
+                nn.Conv2d(1, 32, 5, padding=2),  # batch, 32, 28, 28
+
+                nn.LeakyReLU(0.2, True),
+                nn.AvgPool2d(2, stride=2),  # batch, 32, 14, 14
+            )
+            self.conv2 = nn.Sequential(
+                nn.Conv2d(32, 64, 5, padding=2),  # batch, 64, 14, 14
+
+                nn.BatchNorm2d(64),
+                nn.LeakyReLU(0.2, True),
+                nn.AvgPool2d(2, stride=2)  # batch, 64, 7, 7
+            )
+
+            self.fc = nn.Sequential(
+
+                nn.Linear(4 * h * w, 1024),
+                nn.LeakyReLU(0.2, True),
+                nn.Linear(1024, 1),
+                nn.Sigmoid()
+            )
+
+        def forward(self, x):
+            '''
+            x: batch, width, height, channel=1
+            '''
+            b, c, w, h = x.shape
+            x = self.conv1(x)
+
+            # print(x.shape)
+            x = self.conv2(x)
+            # print(x.shape)
+            x = x.view(x.size(0), -1)
+            # print(x.shape)
+            x = self.fc(x)
+            # print(x.shape)
+            return x
+
+    class discriminator(nn.Module):
+
+        def __init__(self):
+            super(discriminator, self).__init__()
+            self.conv1 = nn.Sequential(
+                nn.Conv2d(1, 64, 4, stride=2, padding=1),  # batch, 64, 48, 48
+                nn.ReLU(0.2)
+            )
+            self.conv2 = nn.Sequential(
+                nn.Conv2d(64, 128, 4, stride=2, padding=1),  # batch, 128, 24, 24
+                nn.BatchNorm2d(128),
+                nn.LeakyReLU(0.2, True)
+
+            )
+            self.conv3 = nn.Sequential(
+                nn.Conv2d(128, 256, 4, stride=2, padding=1),  # batch, 256, 12, 12
+                nn.BatchNorm2d(256),
+                nn.LeakyReLU(0.2, True)
+
+            )
+            self.conv4 = nn.Sequential(
+                nn.Conv2d(256, 512, 4, stride=2, padding=1),  # batch, 512, 6, 6
+                nn.BatchNorm2d(512),
+                nn.LeakyReLU(0.2, True)
+
+            )
+
+            self.fc = nn.Sequential(
+                nn.Linear(2 * h * w, 256),
+                nn.LeakyReLU(0.2, True),
+                nn.Linear(256, 1),
+                nn.Sigmoid()
+            )
+
+        def forward(self, x):
+            '''
+            x: batch, width, height, channel=1
+            '''
+            b, c, w, h = x.shape
+            x = self.conv1(x)
+
+            # print(x.shape)
+            x = self.conv2(x)
+            # print(x.shape)
+            x = self.conv3(x)
+            x = self.conv4(x)
+            x = x.view(x.size(0), -1)
+            # print(x.shape)
+            x = self.fc(x)
+            # print(x.shape)
+            return x
+
+    # class generator(nn.Module):
+    #     def __init__(self, input_size, num_feature):
+    #         super(generator, self).__init__()
+    #         self.fc = nn.Linear(input_size, num_feature)  # batch, 3136=1x56x56
+    #         self.br = nn.Sequential(
+    #             nn.BatchNorm2d(1),
+    #             nn.ReLU(True)
+    #         )
+    #         self.downsample1 = nn.Sequential(
+    #             nn.Conv2d(1, 50, 3, stride=1, padding=1),  # batch, 50, 56, 56
+    #             nn.BatchNorm2d(50),
+    #             nn.ReLU(True)
+    #         )
+    #         self.downsample2 = nn.Sequential(
+    #             nn.Conv2d(50, 25, 3, stride=1, padding=1),  # batch, 25, 56, 56
+    #             nn.BatchNorm2d(25),
+    #             nn.ReLU(True)
+    #         )
+    #         self.downsample3 = nn.Sequential(
+    #             nn.Conv2d(25, 1, 2, stride=2),  # batch, 1, 28, 28
+    #             nn.Tanh()
+    #         )
+    #
+    #     def forward(self, x):
+    #         x = self.fc(x)
+    #         # print(x.shape)
+    #         x = x.view(x.size(0), 1, 96 * 2, 96 * 2)
+    #         # print(x.shape)
+    #         x = self.br(x)
+    #         # print(x.shape)
+    #         x = self.downsample1(x)
+    #         # print(x.shape)
+    #         x = self.downsample2(x)
+    #         # print(x.shape)
+    #         x = self.downsample3(x)
+    #         # print(x.shape)
+    #         return x
+
+    D = discriminator().to(device)  # discriminator model
+    # D=model
+    # G = generator(z_dimension, 36864).to(device)  # generator model
+    G = model.to(device)
+    criterion = nn.BCELoss()  # binary cross entropy
+
+    d_optimizer = torch.optim.Adam(D.parameters(), lr=0.0003)
+    g_optimizer = torch.optim.Adam(G.parameters(), lr=0.0003)
+
+    # train
+    for epoch in range(num_epoch):
+        for i, (img, label) in enumerate(data_loader):
+            num_img = img.size(0)
+            # print("num_img,", num_img)
+            # print(img.shape)
+            # =================train discriminator
+            real_img = Variable(label).to(device)
+            lowhd_img = Variable(img).to(device)
+            real_label = Variable(torch.ones(num_img)).to(device)
+            fake_label = Variable(torch.zeros(num_img)).to(device)
+
+            # compute loss of real_img
+            real_out = D(real_img)
+            # print("done!")
+            d_loss_real = criterion(real_out, real_label)
+            real_scores = real_out  # closer to 1 means better
+            # print("d_loss_real", d_loss_real)
+            # compute loss of fake_img
+            z = Variable(torch.randn(num_img, z_dimension)).to(device)
+            # fake_img = G(z)
+            fake_img = G(lowhd_img)
+            # print("fake_img.shape", fake_img.shape)
+            fake_out = D(fake_img)
+            d_loss_fake = criterion(fake_out, fake_label)
+            fake_scores = fake_out  # closer to 0 means better
+
+            # bp and optimize
+            d_loss = d_loss_real + d_loss_fake
+
+            # print(d_loss)
+
+            d_optimizer.zero_grad()
+            d_loss.backward()
+            d_optimizer.step()
+
+            # ===============train generator
+            # compute loss of fake_img
+            z = Variable(torch.randn(num_img, z_dimension)).to(device)
+            # fake_img = G(z)
+            fake_img = G(lowhd_img)
+            output = D(fake_img)
+            g_loss = criterion(output, real_label)
+
+            # bp and optimize
+            g_optimizer.zero_grad()
+            g_loss.backward()
+            g_optimizer.step()
+
+            if (i + 1) % 10 == 0:
+                print('Epoch [{}/{}], d_loss: {:.6f}, g_loss: {:.6f} '
+                      'D real: {:.6f}, D fake: {:.6f}'
+                      .format(epoch, num_epoch, d_loss.item(), g_loss.item(),
+                              real_scores.data.mean(), fake_scores.data.mean()))
+        if epoch == 0:
+            print('epoch = 0')
+            real_images = to_img(real_img.cpu().data)
+            save_image(real_images, './dc_img/real_images.png')
+            lowhd_imgs = to_img(lowhd_img.cpu().data)
+            save_image(lowhd_imgs, './dc_img/lowhd_img.png')
+        if (epoch + 1) % 10 == 0:
+            fake_images = to_img(fake_img.cpu().data)
+            save_image(fake_images, './dc_img/fake_images-{}.png'.format(epoch + 1))
+        if (epoch + 1) % 50 == 0:
+            torch.save(G.state_dict(), './generator{}.pth'.format(epoch + 1))
+
+    torch.save(G.state_dict(), './generator_final_one.pth')
+    torch.save(D.state_dict(), './discriminator.pth')
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser(description='EarthquakeProject')
+    args.add_argument('-c', '--config', default='config_gan.json', type=str,
+                      help='config file path (default: None)')
+    args.add_argument('-r', '--resume', default=None, type=str,
+                      help='path to latest checkpoint (default: None)')
+    args.add_argument('-d', '--device', default=None, type=str,
+                      help='indices of GPUs to enable (default: all)')
+
+    # custom cli options to modify configuration from default values given in json file.
+    CustomArgs = collections.namedtuple('CustomArgs', 'flags type target')
+    options = [
+        CustomArgs(['--lr', '--learning_rate'], type=float, target='optimizer;args;lr'),
+        CustomArgs(['--bs', '--batch_size'], type=int, target='data_loader;args;batch_size'),
+        CustomArgs(['--ts', '--train_start'], type=int, target='data_loader;args;train_start'),
+        CustomArgs(['--tp', '--train_number_of_pictures'], type=int,
+                   target='data_loader;args;train_number_of_pictures'),
+        CustomArgs(['--ep', '--epochs'], type=int, target='trainer;epochs'),
+        CustomArgs(['--vs', '--val_start'], type=int, target='data_loader;args;val_start'),
+        CustomArgs(['--ts2', '--val_number_of_pictures'], type=int, target='data_loader;args;val_number_of_pictures'),
+
+    ]
+    config = ConfigParser.from_args(args, options)
+    main(config)
+
+
--- Earthquake8/data_loader/dataloader3.py
+++ Earthquake8/data_loader/dataloader3.py
@@ -1,6 +1,6 @@
 
 from functions import *
-
+from scipy.ndimage import filters
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # train/validation/test  用此dataloader
@@ -594,3 +594,376 @@ class EarthquakeDataLoader3dtest():
 
         return test_loader, faults_dataset_test
 
+# gan 用此dataloader
+class EarthquakeDataLoader_gan():
+    def __init__(self, seismic_path, label_path, batch_size, train_number_of_pictures=1, val_number_of_pictures=1,
+                 train_start=1, val_start=0, dimension=0):
+        self.train_loader, self.val_loader = self.train_val_dataset(seismic_path, label_path, batch_size,
+                                                                    train_number_of_pictures, val_number_of_pictures,
+                                                                    train_start, val_start, dimension)
+
+    def train_val_dataset(self, seismic_path, label_path, batch_size, train_number_of_pictures, val_number_of_pictures,
+                          train_start, val_start, dimension):
+        # dimension=1;
+
+        os.environ["CUDA_VISIBLE_DEVICES"] = "0"  # Only GPU 1 is visible to this code
+        time1 = time.time()
+        # print("RandomBrightnessContrast")
+        # modelNo
+        # Unet --> 0
+        # Deeplab --> 1
+        # HED --> 2
+        # RCF --> 3
+        # CED --> 4
+
+        t_start = time.time()
+        seismic = np.load(seismic_path)
+        fault = np.load(label_path)
+        # label=original high resolution
+        fault = seismic
+        img1, img2 = preprocess(seismic)
+        fault = img2
+        # input = low resolution
+        seismic = filters.gaussian_filter(img1, 1)
+        # seismic =  filters.gaussian_filter(seismic,1)
+
+        # print("load in {} sec".format(time.time() - t_start))
+
+        # In[4]:
+
+        # print(seismic.shape, fault.shape)
+        # seismic = np.moveaxis(seismic[4:1807],-2,-1)
+        # print("seismic.shape, fault.shape",seismic.shape, fault.shape)
+        # print("seismic.max(), seismic.min(), fault.max(), fault.min()",seismic.max(), seismic.min(), fault.max(), fault.min())
+        # # reorder input data to same order IL, Z, XL
+
+        # # In[5]:
+
+        # seismic = (seismic-seismic.min(axis=(1,2), keepdims=True))/(seismic.max(axis=(1,2), keepdims=True)-seismic.min(axis=(1,2), keepdims=True))
+        # print(seismic.shape)
+
+        # # In[6]:
+
+        IL, Z, XL = fault.shape
+        if dimension == 0:
+            im_height = Z
+            im_width = XL
+        elif dimension == 1:
+            im_height = IL
+            im_width = XL
+        elif dimension == 2:
+            im_height = IL
+            im_width = Z
+        # im_height = Z
+        # im_width = XL
+        splitsize = 96
+        stepsize = 48
+        overlapsize = splitsize - stepsize
+        pixelThre = int(0.03 * splitsize * splitsize)
+        # print("pixelThre ",pixelThre)
+
+        # In[8]:
+
+        horizontal_splits_number = int(np.ceil((im_width - overlapsize) / stepsize))
+        # print("horizontal_splits_number", horizontal_splits_number)
+        width_after_pad = stepsize * horizontal_splits_number + overlapsize
+        # print("width_after_pad", width_after_pad)
+        left_pad = int((width_after_pad - im_width) / 2)
+        right_pad = width_after_pad - im_width - left_pad
+        # print("left_pad,right_pad", left_pad, right_pad)
+
+        vertical_splits_number = int(np.ceil((im_height - overlapsize) / stepsize))
+        # print("vertical_splits_number", vertical_splits_number)
+        height_after_pad = stepsize * vertical_splits_number + overlapsize
+        # print("height_after_pad", height_after_pad)
+        top_pad = int((height_after_pad - im_height) / 2)
+        bottom_pad = height_after_pad - im_height - top_pad
+        # print("top_pad,bottom_pad", top_pad, bottom_pad)
+
+        # In[17]:
+        # train_dataset
+
+        t_start = time.time()
+        X = []
+        Y = []
+
+        for i in range(train_start, train_number_of_pictures + train_start, 1):
+            if dimension == 0:
+                mask = fault[i]
+            elif dimension == 1:
+                mask = fault[:, i, :]
+            elif dimension == 2:
+                mask = fault[:, :, i]
+            # mask = fault[i]
+            # print("mask:         ",mask.shape)
+            splits = split_Image(mask, True, top_pad, bottom_pad, left_pad, right_pad, splitsize, stepsize,
+                                 vertical_splits_number, horizontal_splits_number)
+            #     print(splits.shape)
+            t = (splits.sum((1, 2)) < pixelThre)
+            no_label_element_index = list(compress(range(len(t)), t))
+            # get all the indexes of the no label pieces by adding elements in axis 2 and 3.
+            splits = np.delete(splits, no_label_element_index, 0)  # delete element i along axis 0
+            #     print("splits.shape", splits.shape)
+            Y.extend(splits)
+
+            if dimension == 0:
+                img = seismic[i]
+            elif dimension == 1:
+                img = seismic[:, i, :]
+            elif dimension == 2:
+                img = seismic[:, :, i]
+            # img = seismic[i]
+
+            splits = split_Image(img, True, top_pad, bottom_pad, left_pad, right_pad, splitsize, stepsize,
+                                 vertical_splits_number, horizontal_splits_number)
+            splits = np.delete(splits, no_label_element_index, 0)  # delete element i along axis 0
+            #     print("splits.shape", splits.shape)
+            X.extend(splits)
+        #     break
+
+        # print("len(Y)",len(Y))
+        # print("len(X)",len(X))
+        # print("X[0].shape",X[0].shape)
+        # print("read images in {} sec".format(time.time() - t_start))
+
+        # In[20]:
+
+        t_start = time.time()
+        X = np.asarray(X, dtype=np.float32)
+        Y = np.asarray(Y, dtype=np.float32)
+        # print(X.shape)
+        # print(Y.shape)
+        # print("read images in {} sec".format(time.time() - t_start))
+
+        # In[22]:
+
+        if len(Y.shape) == 3:
+            Y = np.expand_dims(Y, axis=-1)
+        if len(X.shape) == 3:
+            X = np.expand_dims(X, axis=-1)
+        # print(X.shape)
+        # print(Y.shape)
+
+        X_train = X
+        Y_train = Y
+
+        t_start = time.time()
+        X = []
+        Y = []
+        for i in range(val_start, val_number_of_pictures + val_start, 1):
+            if dimension == 0:
+                mask = fault[i]
+            elif dimension == 1:
+                mask = fault[:, i, :]
+            elif dimension == 2:
+                mask = fault[:, :, i]
+            # mask = fault[i]
+            splits = split_Image(mask, True, top_pad, bottom_pad, left_pad, right_pad, splitsize, stepsize,
+                                 vertical_splits_number, horizontal_splits_number)
+            #     print(splits.shape)
+            t = (splits.sum((1, 2)) < pixelThre)
+            no_label_element_index = list(compress(range(len(t)), t))
+            # get all the indexes of the no label pieces by adding elements in axis 2 and 3.
+            splits = np.delete(splits, no_label_element_index, 0)  # delete element i along axis 0
+            #     print("splits.shape", splits.shape)
+            Y.extend(splits)
+
+            if dimension == 0:
+                img = seismic[i]
+            elif dimension == 1:
+                img = seismic[:, i, :]
+            elif dimension == 2:
+                img = seismic[:, :, i]
+            # img = seismic[i]
+            splits = split_Image(img, True, top_pad, bottom_pad, left_pad, right_pad, splitsize, stepsize,
+                                 vertical_splits_number, horizontal_splits_number)
+            splits = np.delete(splits, no_label_element_index, 0)  # delete element i along axis 0
+            #     print("splits.shape", splits.shape)
+            X.extend(splits)
+        #     break
+
+        # print(len(Y))
+        # print(len(X))
+        # print(X[0].shape)
+        # print("read images in {} sec".format(time.time() - t_start))
+
+        # In[20]:
+
+        t_start = time.time()
+        X = np.asarray(X, dtype=np.float32)
+        Y = np.asarray(Y, dtype=np.float32)
+        # print(X.shape)
+        # print(Y.shape)
+        # print("read images in {} sec".format(time.time() - t_start))
+
+        # In[22]:
+
+        if len(Y.shape) == 3:
+            Y = np.expand_dims(Y, axis=-1)
+        if len(X.shape) == 3:
+            X = np.expand_dims(X, axis=-1)
+        # print(X.shape)
+        # print(Y.shape)
+
+        X_val = X
+        Y_val = Y
+
+        # print("X_train", X_train.shape)
+        # print("X_val", X_val.shape)
+        #
+        # print("Y_train", Y_train.shape)
+        # print("Y_val", Y_val.shape)
+
+        # In[ ]:
+
+        # aug_times = 1
+
+        # t_start = time.time()
+        # origin_train_size = len(X_train)
+        # print(origin_train_size)
+        # X_train_aug = np.zeros((origin_train_size*aug_times,splitsize,splitsize,1))
+        # Y_train_aug = np.zeros((origin_train_size*aug_times,splitsize,splitsize,1))
+        # for i in range(len(X_train)):
+        #     for j in range(aug_times):
+        #         aug = strong_aug(p=1)
+        #         augmented = aug(image=X_train[i], mask=Y_train[i])
+        #         X_train_aug[origin_train_size*j + i] = augmented['image']
+        #         Y_train_aug[origin_train_size*j + i] = augmented['mask']
+        # print("read images in {} sec".format(time.time()-t_start))
+
+        # X_train_aug = X_train_aug.astype(np.float32)
+        # Y_train_aug = Y_train_aug.astype(np.float32)
+        # if len(X_train)==origin_train_size:
+        #     X_train = np.append(X_train,X_train_aug, axis=0)
+        # if len(Y_train)==origin_train_size:
+        #     Y_train = np.append(Y_train, Y_train_aug, axis=0)
+        # print("X_train after aug",X_train.shape)
+        # print("Y_train after aug",Y_train.shape)
+        # print("read images in {} sec".format(time.time()-t_start))
+        # X_train = X_train.astype(np.float32)
+        # Y_train = Y_train.astype(np.float32)
+        # #-----------------------
+        # t_start = time.time()
+        # origin_val_size = len(X_val)
+        # print(origin_val_size)
+        # X_val_aug = np.zeros((origin_val_size*aug_times,splitsize,splitsize,1))
+        # Y_val_aug = np.zeros((origin_val_size*aug_times,splitsize,splitsize,1))
+        # for i in range(len(X_val)):
+        #     for j in range(aug_times):
+        #         aug = strong_aug(p=1)
+        #         augmented = aug(image=X_train[i], mask=Y_train[i])
+        #         X_val_aug[origin_val_size*j + i] = augmented['image']
+        #         Y_val_aug[origin_val_size*j + i] = augmented['mask']
+        # print("read images in {} sec".format(time.time()-t_start))
+
+        # X_val_aug = X_val_aug.astype(np.float32)
+        # Y_val_aug = Y_val_aug.astype(np.float32)
+        # if len(X_val)==origin_val_size:
+        #     X_val = np.append(X_val,X_val_aug, axis=0)
+        # if len(Y_val)==origin_val_size:
+        #     Y_val = np.append(Y_val, Y_val_aug, axis=0)
+        # print("X_val after aug",X_val.shape)
+        # print("Y_val after aug",Y_val.shape)
+        # print("read images in {} sec".format(time.time()-t_start))
+        # X_val = X_val.astype(np.float32)
+        # Y_val = Y_val.astype(np.float32)
+        # -----------------------
+        print("X_train\n\n\n", X_train.shape)
+        X_train = np.moveaxis(X_train, -1, 1)
+        Y_train = np.moveaxis(Y_train, -1, 1)
+        X_val = np.moveaxis(X_val, -1, 1)
+        Y_val = np.moveaxis(Y_val, -1, 1)
+
+        # print("X_train", X_train.shape)
+        # print("X_val", X_val.shape)
+        # print("Y_train", Y_train.shape)
+        # print("Y_val", Y_val.shape)
+
+        # In[ ]:
+
+        # dataset
+        # idea from: https://www.kaggle.com/erikistre/pytorch-basic-u-net
+
+        class faultsDataset(torch.utils.data.Dataset):
+
+            def __init__(self, preprocessed_images, train=True, preprocessed_masks=None):
+                """
+                Args:
+                    text_file(string): path to text file
+                    root_dir(string): directory with all train images
+                """
+                self.train = train
+                self.images = preprocessed_images
+                self.masks = preprocessed_masks
+
+            def __len__(self):
+                return len(self.images)
+
+            def __getitem__(self, idx):
+                image = self.images[idx]
+                #         mask = None
+                # #         if self.train:
+                mask = self.masks[idx]
+                return (image, mask)
+
+        # In[32]:
+
+        faults_dataset_train = faultsDataset(X_train, train=True, preprocessed_masks=Y_train)
+        faults_dataset_val = faultsDataset(X_val, train=False, preprocessed_masks=Y_val)
+        # faults_dataset_test = faultsDataset(X_test, train=False, preprocessed_masks=Y_test)
+
+        train_loader = torch.utils.data.DataLoader(dataset=faults_dataset_train,
+                                                   batch_size=batch_size,
+                                                   shuffle=True)
+
+        val_loader = torch.utils.data.DataLoader(dataset=faults_dataset_val,
+                                                 batch_size=batch_size,
+                                                 shuffle=False)
+
+        # test_loader = torch.utils.data.DataLoader(dataset=faults_dataset_test,
+        #                                            batch_size=batch_size,
+        #                                            shuffle=False)
+        return train_loader, val_loader
+
+def preprocess(image, scale=3):
+    """
+    Preprocess single image file
+      (1) Read original image as YCbCr format (and grayscale as default)
+      (2) Normalize
+      (3) Apply image file with bicubic interpolation
+    Args:
+      path: file path of desired file
+      input_: image applied bicubic interpolation (low-resolution)
+      label_: image with original resolution (high-resolution)
+    """
+    # image = imread(path, is_grayscale=True)
+    label_ = modcrop(image, scale)
+
+    # # Must be normalized
+    # image = image / 255.
+    # label_ = label_ / 255.
+
+    input_ = scipy.ndimage.interpolation.zoom(label_, (1. / scale), prefilter=False)
+    input_ = scipy.ndimage.interpolation.zoom(input_, (scale / 1.), prefilter=False)
+
+    return input_, label_
+
+def modcrop(image, scale=3):
+    """
+    To scale down and up the original image, first thing to do is to have no remainder while scaling operation.
+
+    We need to find modulo of height (and width) and scale factor.
+    Then, subtract the modulo from height (and width) of original image size.
+    There would be no remainder even after scaling operation.
+    """
+    if len(image.shape) == 3:
+        h, w, _ = image.shape
+        h = h - np.mod(h, scale)
+        w = w - np.mod(w, scale)
+        image = image[0:h, 0:w, :]
+    else:
+        h, w = image.shape
+        h = h - np.mod(h, scale)
+        w = w - np.mod(w, scale)
+        image = image[0:h, 0:w]
+    return image
