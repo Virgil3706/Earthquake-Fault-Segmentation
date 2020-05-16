a9662de7819945efd7465b183fe2db0760276493
maxiaoxi12345 <62084159+maxiaoxi12345@users.noreply.github.com>
2020-05-16 16:21:41 +0800

    Update xception.py

--- Earthquake8/model_zoo/DEEPLAB/backbone/xception.py
+++ Earthquake8/model_zoo/DEEPLAB/backbone/xception.py
@@ -95,8 +95,8 @@ class AlignedXception(nn.Module):
     """
     Modified Alighed Xception
     """
-    def __init__(self, output_stride, BatchNorm,
-                 pretrained=True):
+    def __init__(self, output_stride, BatchNorm,pretrained=False):
+              #   pretrained=True):
         super(AlignedXception, self).__init__()
 
         if output_stride == 16:
@@ -112,7 +112,9 @@ class AlignedXception(nn.Module):
 
 
         # Entry flow
-        self.conv1 = nn.Conv2d(3, 32, 3, stride=2, padding=1, bias=False)
+        #self.conv1 = nn.Conv2d(3, 32, 3, stride=2, padding=1, bias=False)
+
+        self.conv1 = nn.Conv2d(1, 32, 1, stride=2, padding=1, bias=False)
         self.bn1 = BatchNorm(32)
         self.relu = nn.ReLU(inplace=True)
 
@@ -281,7 +283,9 @@ class AlignedXception(nn.Module):
 
 if __name__ == "__main__":
     import torch
-    model = AlignedXception(BatchNorm=nn.BatchNorm2d, pretrained=True, output_stride=16)
+
+    #model = AlignedXception(BatchNorm=nn.BatchNorm2d, pretrained=True, output_stride=16)
+    model = AlignedXception(BatchNorm=nn.BatchNorm2d, pretrained=False, output_stride=16)
     input = torch.rand(1, 3, 512, 512)
     output, low_level_feat = model(input)
     print(output.size())
