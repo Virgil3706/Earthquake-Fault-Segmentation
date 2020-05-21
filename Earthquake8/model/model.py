2677e035fbb0d286adbb472e9a9becc41e6d85d6
taoshutong <62104815+taoshutong@users.noreply.github.com>
2020-05-21 21:23:49 +0800

    Update model.py

--- Earthquake8/model/model.py
+++ Earthquake8/model/model.py
@@ -1261,3 +1261,116 @@ class hed_model(nn.Module):
                 if i == j:
                     w[i, j] = filt
         return w
+
+#U-net 3D    
+def conv_block_3d(in_dim, out_dim, activation):
+    return nn.Sequential(
+        nn.Conv3d(in_dim, out_dim, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm3d(out_dim),
+        activation,)
+
+
+def conv_trans_block_3d(in_dim, out_dim, activation):
+    return nn.Sequential(
+        nn.ConvTranspose3d(in_dim, out_dim, kernel_size=3, stride=2, padding=1, output_padding=1),
+        nn.BatchNorm3d(out_dim),
+        activation, )
+
+
+def max_pooling_3d():
+    return nn.MaxPool3d(kernel_size=2, stride=2, padding=0)
+
+
+def conv_block_2_3d(in_dim, out_dim, activation):
+    return nn.Sequential(
+        conv_block_3d(in_dim, out_dim, activation),
+        nn.Conv3d(out_dim, out_dim, kernel_size=3, stride=1, padding=1),
+        nn.BatchNorm3d(out_dim), )
+
+
+class unet3d(nn.Module):
+    def __init__(self):
+        super(unet3d, self).__init__()
+
+        num_filters = 4
+        # activation = nn.LeakyReLU(0.2, inplace=True)
+        activation = nn.ReLU(inplace=True)
+
+        # Down sampling
+        self.down_1 = conv_block_2_3d(1, num_filters, activation)
+        self.pool_1 = max_pooling_3d()
+        self.down_2 = conv_block_2_3d(num_filters, num_filters * 2, activation)
+        self.pool_2 = max_pooling_3d()
+        self.down_3 = conv_block_2_3d(num_filters * 2, num_filters * 4, activation)
+        self.pool_3 = max_pooling_3d()
+        self.down_4 = conv_block_2_3d(num_filters * 4, num_filters * 8, activation)
+        self.pool_4 = max_pooling_3d()
+        self.down_5 = conv_block_2_3d(num_filters * 8, num_filters * 16, activation)
+        self.pool_5 = max_pooling_3d()
+
+        # Bridge
+        self.bridge = conv_block_2_3d(num_filters * 16, num_filters * 32, activation)
+
+        # Up sampling
+        self.trans_1 = conv_trans_block_3d(num_filters * 32, num_filters * 32, activation)
+        self.up_1 = conv_block_2_3d(num_filters * 48, num_filters * 16, activation)
+        self.trans_2 = conv_trans_block_3d(num_filters * 16, num_filters * 16, activation)
+        self.up_2 = conv_block_2_3d(num_filters * 24, num_filters * 8, activation)
+        self.trans_3 = conv_trans_block_3d(num_filters * 8, num_filters * 8, activation)
+        self.up_3 = conv_block_2_3d(num_filters * 12, num_filters * 4, activation)
+        self.trans_4 = conv_trans_block_3d(num_filters * 4, num_filters * 4, activation)
+        self.up_4 = conv_block_2_3d(num_filters * 6, num_filters * 2, activation)
+        self.trans_5 = conv_trans_block_3d(num_filters * 2, num_filters * 2, activation)
+        self.up_5 = conv_block_2_3d(num_filters * 3, num_filters * 1, activation)
+
+        # Output
+        self.out = conv_block_3d(num_filters, 1, nn.Sigmoid())
+
+    def forward(self, x):
+        # Down sampling
+        down_1 = self.down_1(x)  
+        pool_1 = self.pool_1(down_1)  
+
+        down_2 = self.down_2(pool_1) 
+        pool_2 = self.pool_2(down_2) 
+
+        down_3 = self.down_3(pool_2) 
+        pool_3 = self.pool_3(down_3)  
+
+        down_4 = self.down_4(pool_3) 
+        pool_4 = self.pool_4(down_4) 
+
+        down_5 = self.down_5(pool_4) 
+        pool_5 = self.pool_5(down_5) 
+
+        # Bridge
+        bridge = self.bridge(pool_5)  
+
+        # Up sampling
+        trans_1 = self.trans_1(bridge) 
+        concat_1 = torch.cat([trans_1, down_5], dim=1) 
+        up_1 = self.up_1(concat_1)
+
+        trans_2 = self.trans_2(up_1)  
+        concat_2 = torch.cat([trans_2, down_4], dim=1) 
+        up_2 = self.up_2(concat_2) 
+
+        trans_3 = self.trans_3(up_2)  
+        concat_3 = torch.cat([trans_3, down_3], dim=1)  
+        up_3 = self.up_3(concat_3) 
+
+        trans_4 = self.trans_4(up_3)  
+        concat_4 = torch.cat([trans_4, down_2], dim=1)  
+        up_4 = self.up_4(concat_4) 
+
+        trans_5 = self.trans_5(up_4) 
+        concat_5 = torch.cat([trans_5, down_1], dim=1)
+        up_5 = self.up_5(concat_5) 
+
+        # Output
+        out = self.out(up_5)  
+        return out
+
+
+
+    
