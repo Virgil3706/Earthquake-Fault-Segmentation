c4237e54e7109ad2294f692b9d3150b57ee46bdb
taoshutong <62104815+taoshutong@users.noreply.github.com>
2020-05-21 21:19:02 +0800

    Update metric.py

--- Earthquake8/model/metric.py
+++ Earthquake8/model/metric.py
@@ -40,6 +40,41 @@ def accuracy1(outputs,labels):
     acc = (weight_1*TP + weight_2*TN) / (weight_1*label_true+weight_2*label_false)
     return acc
 
+def IoU3d(predicted_mask, masks):
+    c =iou_pytorch1(predicted_mask, masks)
+    return torch.mean(c)
+
+def accuracy3d(outputs,labels):
+    # acc = ((outputs==labels).sum().item() )/ ((outputs==labels).sum().item()+(outputs!=labels).sum().item())
+    TP = 0
+    TN = 0
+    FP = 0
+    FN = 0
+    # TP    predict 和 label 同时为1
+    TP += ((outputs == 1) & (labels == 1)).sum().item()
+    # TN    predict 和 label 同时为0
+    TN += ((outputs == 0) & (labels == 0)).sum().item()
+    # FN    predict 0 label 1
+    FN += ((outputs == 0) & (labels == 1)).sum().item()
+    # FP    predict 1 label 0
+    FP += ((outputs == 1) & (labels == 0)).sum().item()
+    label_true = (labels == 1).sum().item()
+    label_false = (labels == 0).sum().item()
+
+    weight_1 = label_false/(label_false + label_true)
+    weight_2 = label_true/(label_false + label_true)
+
+    # print("weight_1",weight_1)
+    # print("label_true",label_true)
+    # print("TP",TP)
+    # print("output=1",(outputs == 1).sum().item())
+
+    total=TP + TN + FP + FN
+    # acc = (TP + TN) / (TP + TN + FP + FN)
+    acc = (weight_1*TP + weight_2*TN) / (weight_1*label_true+weight_2*label_false)
+    return acc
+
+
 def TPR(outputs,labels):
     outputs=outputs.squeeze(1).byte()
     labels=labels.squeeze(1).byte()
